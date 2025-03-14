236cf90b511f0d93c195d163d37bd41dc7fda8b3
liruiluo <l1176358062@gmail.com>
2025-03-14 17:04:33 +0800

    del SPS print

--- experiments/meta-world/run_sac.py
+++ experiments/meta-world/run_sac.py
@@ -446,7 +446,7 @@ if __name__ == "__main__":
                 writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
                 writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
                 writer.add_scalar("losses/alpha", alpha, global_step)
-                print("SPS:", int(global_step / (time.time() - start_time)))
+                # print("SPS:", int(global_step / (time.time() - start_time)))
                 writer.add_scalar(
                     "charts/SPS",
                     int(global_step / (time.time() - start_time)),
